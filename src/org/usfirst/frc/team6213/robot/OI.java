286cbc0c3149b8f9e1edfef062056c5712cf60df
James <jcummins@healytics.com>
2018-01-15 14:30:20 -0500

    First Commit

---
+++ src/org/usfirst/frc/team6213/robot/OI.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team6213.robot;
+
+// Holds all input devices and provides methods for accessing input
+
+public class OI {
+
+}
---
+++ src/org/usfirst/frc/team6213/robot/components/DriveTrain.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team6213.robot.components;
+
+// Provides access to the robot's drive train, composed of the 2 driving motors
+
+public class DriveTrain {
+
+}
