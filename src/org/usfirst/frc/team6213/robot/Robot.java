cc6b4db83cbce9f60281052fa5a2b27fe7c1abb7
Mohammed <m.m.akinbayo@gmail.com>
2018-02-06 16:32:52 -0500

    Updated Controls
    -added xbox control

--- src/org/usfirst/frc/team6213/robot/Robot.java
+++ src/org/usfirst/frc/team6213/robot/Robot.java
@@ -1,130 +1,95 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team6213.robot;
-
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import java.awt.Image;
+import javax.swing.JFrame;
+import org.opencv.videoio.VideoCapture;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 //import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+
 public class Robot extends IterativeRobot {
-	private DifferentialDrive m_robotDrive
+	private DifferentialDrive robotDrive
 			= new DifferentialDrive(new Spark(0), new Spark(1));
+	//private Spark motor1 = new Spark(0);
 	//private Spark motor2 = new Spark(1);
 	private Spark control = new Spark(2);
-	private Joystick m_stick = new Joystick(0);
-	private Joystick joystick2 = new Joystick(1);
+	private XboxController xbox = new XboxController(0);
+	private Joystick joystick = new Joystick(1);
 	private Timer m_timer = new Timer();
+	private Solenoid sole = new Solenoid(0);
+	private Compressor compressor = new Compressor(1);
 	boolean climberFlag = false;
 	JoystickButton aButton;
 	JoystickButton bButton;
 	JoystickButton triggerJoystick;
+	double rTrigger;
+	double lTrigger;
+	double Triggers; 
 	
 	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 		
 		UsbCamera cam = CameraServer.getInstance().startAutomaticCapture();
+		VideoCapture vidCam = new VideoCapture(0);
+		
 		
 	}
-
-	/**
-	 * This function is run once each time the robot enters autonomous mode.
-	 */
+	
 	@Override
 	public void autonomousInit() {
 		m_timer.reset();
 		m_timer.start();
+		
+		
+		
+		
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		// Drive for 2 seconds
 		if (m_timer.get() < 2.0) {
-			m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+			robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
 		} else {
-			m_robotDrive.stopMotor(); // stop robot
+			robotDrive.stopMotor(); // stop robot
 		}
 	}
 
-	/**
-	 * This function is called once each time the robot enters teleoperated mode.
-	 */
+
 	@Override
 	public void teleopInit() {
 	}
 
-	/**
-	 * This function is called periodically during teleoperated mode.
-	 */
 	@Override
 	public void teleopPeriodic() {
-		m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+		rTrigger = xbox.getRawAxis(3);
+		lTrigger = -1*xbox.getRawAxis(2);
+		Triggers = rTrigger + lTrigger;
+		robotDrive.arcadeDrive(Triggers, xbox.getX());
+		//robotDrive.arcadeDrive(joystick.getY(), joystick.getX());
 		
-		boolean aButton = m_stick.getRawButton(1);
-		boolean bButton = m_stick.getRawButton(2);
-		boolean triggerJoystick = joystick2.getRawButton(1);
-		
-		/*if(aButton) {
-			if (!climberFlag) {
-				control.set(1.0);
-				climberFlag = true;
-			}
-		}else if (climberFlag){
-			control.set(0.0);
-			climberFlag = false;
-		}*/
-		
-		
-		if(triggerJoystick){
-			control.set(1.0);
-		}else {
-			control.set(0.0);
-		}
-		
-		if(aButton) {
-			control.set(1.0);
-		}else {
-			control.set(0.0);
-		}
-		
-		if(bButton) {
-			control.set(-1.0);
-		}else {
-			control.set(0.0);
-		}
+		boolean aButton = xbox.getRawButton(1);
+		boolean bButton = xbox.getRawButton(2);
+		boolean xButton = xbox.getRawButton(3);
+		boolean yButton = xbox.getRawButton(4);
+		boolean triggerJoystick = joystick.getRawButton(1);
+
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
